18dbbc3f29b79b0c3f04edeae5225cd5a750e73f
Eik Meng <leongem-wa15@student.tarc.edu.my>
2018-11-13 13:11:51 +0800

    Initial commit

---
+++ app/src/main/java/com/eikmeng/lab3_2resouces/MainActivity.java
@@ -0,0 +1,25 @@
+package com.eikmeng.lab3_2resouces;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView textViewMessage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textViewMessage = findViewById(R.id.textviewMessage);
+
+    }
+
+    public void showMessage(View view){
+        textViewMessage.setText(getString(R.string.greeting));
+    }
+
+}
